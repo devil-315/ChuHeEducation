ffb04fedcb3222a4f683b4a3a91e9c75e7855ee6
devil <3223291384@qq.com>
2025-01-14 12:34:30 +0800

    nacos配置

---
+++ ChuHuEducation-media/ChuHeEducation-media-service/src/main/java/com/education/media/mapper/MediaFilesMapper.java
@@ -0,0 +1,15 @@
+package com.education.media.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.education.media.model.po.MediaFiles;
+
+/**
+ * <p>
+ * 媒资信息 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MediaFilesMapper extends BaseMapper<MediaFiles> {
+
+}
